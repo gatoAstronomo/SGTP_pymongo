417d2087265f2fc763f2bd74ef244b38a8b8e04f
Arturo <a.rivas06@ufromail.cl>
2024-04-02 01:48:54 -0300

    Servidor Envia codigos de estado 400 cuando no es posible crear el usuario

--- src/server.py
+++ src/server.py
@@ -13,7 +13,7 @@ def create_user():
     # Receiving data
     rut = request.json['rut']
     if not rut:
-        return {'message': 'Proporcione un rut'}
+        return {'message': 'Proporcione un rut'}, 400
 
     # Revisa si existe el rut en la base de datos
     exist_rut = mongo.db.users.find_one({'rut': rut})
@@ -28,7 +28,7 @@ def create_user():
         return response
 
     else:
-        return {'message': 'El rut ya existe'}
+        return {'message': 'El rut ya existe'}, 400
     
 
 if __name__ == "__main__":
