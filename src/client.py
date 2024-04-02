d50589f83d1d1e963b56e4d77cbbb59699d7c640
Arturo <a.rivas06@ufromail.cl>
2024-04-02 03:04:53 -0300

    Mejora la verificación de rut

--- src/client.py
+++ src/client.py
@@ -10,13 +10,36 @@ def create_user(rut: str):
     data = {'rut': rut}    
     return post(f'{URL}/users', json=data)
 
+def extraer_numeros(cadena):
+    numeros_str = ""
+    for caracter in cadena:
+        if '0' <= caracter <= '9':  # Verifica si el carácter es un dígito numérico
+            numeros_str += caracter
+    return numeros_str
+
+def validar_rut(rut: str):
+    rut = extraer_numeros(rut)
+    
+    # Longitud correcta
+    if len(rut) != 9:
+        return False
+    
+    try:
+        return rut_chile.is_valid_rut(rut)
+    except:
+        return False
+    
+
 def main():
-    rut = "85742345"
-    if rut_chile.is_valid_rut(rut):
+    rut = "21.512.921-7"
+    print(validar_rut(rut))
+    print(rut_chile.format_capitalized_rut_with_dots(rut))
+    """
+    if validar_rut(rut):
         response = create_user(rut)
         print(response.json())
     else:
-        print("Ingrese un rut valido")
+        print("Ingrese un rut valido")"""
     
     
     
